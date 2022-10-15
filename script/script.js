d756439affb1f1de23e172dde0a2e26bc127e4b1
Nishiyama13 <alicia_nishiyama>
2022-10-15 07:39:05 -0300

    versão layout colorida

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function pratoPrincipal() {
+  alert("Falta definir esta função no js!!! pratoPrincipal");
+}
+
+function bebida() {
+  alert("Falta definir esta função no js!!! bebida ");
+}
+function sobremesa() {
+  alert("Falta definir esta função no js!!! sobremesa ");
+}
